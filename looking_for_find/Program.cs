032f650a9be0778337544b3713154d2d85b9f10a
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-11-22 20:43:22 +0300

    add tasks from Lection 2

---
+++ looking_for_find/Program.cs
@@ -0,0 +1,19 @@
+﻿// Ищем в массиве данных елемент find
+
+int [] array = { 1, 12, 31, 4, 1, 15, 16, 17, 18 };
+
+int n = array.Length;
+int find = 1;
+
+int index = 0;
+
+while (index < n)
+{
+
+    if(array[index] == find)    // это значит эквивалент левой части и правой
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++; // index = index + 1;
+}
---
+++ max from 9 with massiv/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ищем максимум из 9 чисел используя массив данных
+
+int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if (arg2 > result) result = arg2;
+    if (arg3 > result) result = arg3;
+    return result;
+}
+//               0 1    2   3   4   5   6   7   8
+int[] array = { 90, 23, 63, 14, 24, 56, 72, 42, 60 };
+
+int result = Max(
+    Max(array, b1, c1), 
+    Max(а2, b2, c2), 
+    Max(a3, b3, c3));
+Console.WriteLine(result);
