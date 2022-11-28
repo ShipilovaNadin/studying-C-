0725c8169b99a0c5577ddf2e7a7bb5de6497ae1a
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-11-28 22:37:30 +0300

    check 9 task

--- max from 9 with massiv/Program.cs
+++ max from 9 with massiv/Program.cs
@@ -8,10 +8,10 @@ int Max(int arg1, int arg2, int arg3)
     return result;
 }
 //               0 1    2   3   4   5   6   7   8
-int[] array = { 90, 23, 63, 14, 24, 56, 72, 42, 60 };
+int[] array = { 90, 23, 63, 14, 240, 56, 72, 42, 60 };
 
 int result = Max(
-    Max(array, b1, c1), 
-    Max(а2, b2, c2), 
-    Max(a3, b3, c3));
+    Max(array [0], array [1], array [2]), 
+    Max(array [3], array [4], array [5]), 
+    Max(array [6], array [7], array [8]));
 Console.WriteLine(result);
