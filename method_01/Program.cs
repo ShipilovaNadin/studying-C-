e8aeabd55790e7389df0c538a0fa5adf9f60ad4f
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-11-29 20:35:29 +0300

    add 3 task

---
+++ method_01/Program.cs
@@ -0,0 +1,14 @@
+﻿// отрабатываем цикл for
+
+string Method4(int count, string text);
+{   
+    string result = string.Empty; // мы вводим строчку куда будет вноситься результат и пока она пустая. Так это можно записать красиво
+    for(int i = 0; i < count; i++)
+    {
+        result = result + text;
+    }
+    return result;
+}
+
+string res = Method4(10, "z");
+Console.WriteLine(res);
