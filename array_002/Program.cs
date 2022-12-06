ecc46a55f228d5d118b679443741174a65b53304
Nadezhda Shipilova <Nadin.v.sh@gmail.com>
2022-12-06 22:04:03 +0300

    рекурсия и двумерные массивы

---
+++ array_002/Program.cs
@@ -0,0 +1,31 @@
+﻿// заполняем и выводим на печать двумерный массив
+
+void PrintArray(int[,] matr) //задаем функцию переменной которой двумерный массив матр
+{
+    for (int i = 0; i< matr.GetLength(0); i++)  // первый цикл проходится по строкам ->(0)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++) // второй цикл проходится по столбцам -> (1)
+        {
+            Console.Write($"{matr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = new Random().Next(1,10);
+        }
+    }
+}
+
+int [,] matrix = new int[3, 4];  // задаем новый массив с 3 строками и 4 столбцами
+
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
---
+++ factorial/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вычисляем факториал с помощью рекурсии (5!=5*4*3*2*1)
+
+double Factorial (int n)
+{
+    if(n == 1) return 1; // если дойдем до 1 возвращаем 1 так как факториал 1 = 1
+    else return n * Factorial(n - 1);
+}
+
+//Console.WriteLine(Factorial(5));
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
